3f48d246f98462efd0f9b257aed9a4eae27c0acd
Frank Horrigan <redmmond@gmail.com>
2019-11-23 22:38:16 +1000

    Add upload

---
+++ api/put.ts
@@ -0,0 +1,20 @@
+import { protect } from '../lib/auth';
+import { uploadBlobBuffer } from '../lib/client';
+
+export default protect(async (req, res) => {
+    const result = await uploadBlobBuffer(
+        req.body.name,
+        new Buffer(req.body.data, 'base64')
+    );
+
+    return res.status(200).json({
+        success: true,
+        data: {
+            name: req.body.name,
+            creationTime: result.creationTime,
+            lastModified: result.lastModified,
+            etag: result.etag,
+            contentLength: result.contentLength
+        }
+    });
+});
--- lib/client.ts
+++ lib/client.ts
@@ -1,23 +1,40 @@
 import { BlobService } from 'azure-storage';
+import { Readable } from 'stream';
 
 const containerName = 'default';
 const client = new BlobService(process.env.azure_secret!);
 
-// export const uploadBlob = (name: string, fileName: string) =>
-//     new Promise((resolve, reject) => {
-//         client.createBlockBlobFromLocalFile(
-//             containerName,
-//             name,
-//             fileName,
-//             (err, result) => {
-//                 if (err) {
-//                     reject(err);
-//                 } else {
-//                     resolve(result);
-//                 }
-//             }
-//         );
-//     });
+export const uploadBlobBuffer = async (name: string, buffer: Buffer) => {
+    const stream = new Readable();
+    stream._read = () => {
+        /* NOP */
+    };
+    stream.push(buffer);
+    stream.push(null);
+
+    return await uploadBlobStream(name, stream, buffer.length);
+};
+
+export const uploadBlobStream = (
+    name: string,
+    stream: Readable,
+    streamLength: number
+) =>
+    new Promise<BlobService.BlobResult>((resolve, reject) => {
+        client.createBlockBlobFromStream(
+            containerName,
+            name,
+            stream,
+            streamLength,
+            (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+        );
+    });
 
 export const listBlobs = () =>
     new Promise<BlobService.ListBlobsResult>((resolve, reject) => {
